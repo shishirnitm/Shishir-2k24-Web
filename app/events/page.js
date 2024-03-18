925c9219b5b62ccc5b2b1521f4ad561288c4af36
Isaiah Khonglah <isaiahlemiker@gmail.com>
2024-03-18 23:23:55 +0530

    Added Events Filter

--- app/events/page.js
+++ app/events/page.js
@@ -28,16 +28,54 @@ export default function Events() {
         <div className="flex justify-center font-bernier text-justify text-[#E03932] text-[32px] w-5/6 mt-10">
             <div className="w-80">EVENTS ARE DISPLAYED BY POPULARITY!</div>
         </div>
-        {/* <div className="flex border-[1px] border-black bg-white w-5/6 h-12 my-8 px-4 items-center">
-          <div className=" w-1/2 font-blogger">showing all events</div>
-          <div className=" w-1/2 text-end"><u>filters</u></div>
-        </div> */}
+        <div className="flex flex-col border-[1px] border-black bg-white w-5/6 h-44 my-8 px-4 items-center">
+          <div className="flex w-full my-2">
+            <div className=" w-1/2 font-blogger">event categories</div>
+            <div className=" w-1/2 text-end"><u>filters</u></div>
+          </div>
+          <div className="flex flex-wrap w-full gap-x-2 space-y-2 place-items-start text-center">
+            <div className="rounded-2xl border-[1px] border-black font-blogger px-2 h-7 self-end"><button onClick={()=>{setCategory("all")}}>All</button></div>
+            <div className="rounded-2xl border-[1px] border-black font-blogger px-2 h-7"><button onClick={()=>{setCategory("Main")}}>Main</button></div>
+            <div className="rounded-2xl border-[1px] border-black font-blogger px-2 h-7"><button onClick={()=>{setCategory("Music")}}>Music</button></div>
+            <div className="rounded-2xl border-[1px] border-black font-blogger px-2 h-7 col-span-2"><button onClick={()=>{setCategory("EBSB")}}>EBSB Club</button></div>
+            <div className="rounded-2xl border-[1px] border-black font-blogger px-2 h-7 col-span-2"><button onClick={()=>{setCategory("Literary")}}>Literary Club</button></div>
+            <div className="rounded-2xl border-[1px] border-black font-blogger px-2 h-7"><button onClick={()=>{setCategory("Krigg")}}>Krigg</button></div>
+            <div className="rounded-2xl border-[1px] border-black font-blogger px-2 h-7 col-span-2"><button onClick={()=>{setCategory("TF")}}>Task Force Club</button></div>
+            <div className="rounded-2xl border-[1px] border-black font-blogger px-2 h-7 col-span-2"><button onClick={()=>{setCategory("DandD")}}>Dance and Drama Club</button></div>
+            <div className="rounded-2xl border-[1px] border-black font-blogger px-2 h-7 col-span-2"><button onClick={()=>{setCategory("Photography")}}>Photography Club</button></div>
+          </div>
+          
+        </div>
         <div className="flex flex-col w-5/6 space-y-5">
           {category=="all" && (allEvents.map((prop)=>(
             <EventCard key={prop.eName} eName={prop.eName} eTime={prop.eTime} eRule={prop.eRule} eLink={prop.eLink} eDesc={prop.eDesc} eType={prop.eType} eVenue={prop.eVenue} eDay={prop.eDay}/>
           )))}
+          {category=="Main" && (allEvents.map((prop)=>(
+            prop.category=="Main"&&(<EventCard key={prop.eName} eName={prop.eName} eTime={prop.eTime} eRule={prop.eRule} eLink={prop.eLink} eDesc={prop.eDesc} eType={prop.eType} eVenue={prop.eVenue} eDay={prop.eDay}/>)
+          )))}
           {category=="Music" && (allEvents.map((prop)=>(
             prop.category=="Music"&&(<EventCard key={prop.eName} eName={prop.eName} eTime={prop.eTime} eRule={prop.eRule} eLink={prop.eLink} eDesc={prop.eDesc} eType={prop.eType} eVenue={prop.eVenue} eDay={prop.eDay}/>)
+          )))}
+          {category=="Krigg" && (allEvents.map((prop)=>(
+            prop.category=="Krigg"&&(<EventCard key={prop.eName} eName={prop.eName} eTime={prop.eTime} eRule={prop.eRule} eLink={prop.eLink} eDesc={prop.eDesc} eType={prop.eType} eVenue={prop.eVenue} eDay={prop.eDay}/>)
+          )))}
+          {category=="Literary" && (allEvents.map((prop)=>(
+            prop.category=="Literary"&&(<EventCard key={prop.eName} eName={prop.eName} eTime={prop.eTime} eRule={prop.eRule} eLink={prop.eLink} eDesc={prop.eDesc} eType={prop.eType} eVenue={prop.eVenue} eDay={prop.eDay}/>)
+          )))}
+          {category=="Gaming" && (allEvents.map((prop)=>(
+            prop.category=="Gaming"&&(<EventCard key={prop.eName} eName={prop.eName} eTime={prop.eTime} eRule={prop.eRule} eLink={prop.eLink} eDesc={prop.eDesc} eType={prop.eType} eVenue={prop.eVenue} eDay={prop.eDay}/>)
+          )))}
+          {category=="EBSB" && (allEvents.map((prop)=>(
+            prop.category=="EBSB"&&(<EventCard key={prop.eName} eName={prop.eName} eTime={prop.eTime} eRule={prop.eRule} eLink={prop.eLink} eDesc={prop.eDesc} eType={prop.eType} eVenue={prop.eVenue} eDay={prop.eDay}/>)
+          )))}
+          {category=="TF" && (allEvents.map((prop)=>(
+            prop.category=="TF"&&(<EventCard key={prop.eName} eName={prop.eName} eTime={prop.eTime} eRule={prop.eRule} eLink={prop.eLink} eDesc={prop.eDesc} eType={prop.eType} eVenue={prop.eVenue} eDay={prop.eDay}/>)
+          )))}
+          {category=="DandD" && (allEvents.map((prop)=>(
+            prop.category=="DandD"&&(<EventCard key={prop.eName} eName={prop.eName} eTime={prop.eTime} eRule={prop.eRule} eLink={prop.eLink} eDesc={prop.eDesc} eType={prop.eType} eVenue={prop.eVenue} eDay={prop.eDay}/>)
+          )))}
+          {category=="Photography" && (allEvents.map((prop)=>(
+            prop.category=="Photography"&&(<EventCard key={prop.eName} eName={prop.eName} eTime={prop.eTime} eRule={prop.eRule} eLink={prop.eLink} eDesc={prop.eDesc} eType={prop.eType} eVenue={prop.eVenue} eDay={prop.eDay}/>)
           )))}
         </div>
         
